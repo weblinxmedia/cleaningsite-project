df3e84feff95294291be3f9805f05b4c96d1fcb5
Weblinx Media <admin@weblinxmedia.com>
2026-07-21 22:38:19 +0500

    new changes image size

--- components/WhyChooseUs/WhyChooseImage.tsx
+++ components/WhyChooseUs/WhyChooseImage.tsx
@@ -4,12 +4,12 @@ import { useScrollAnimation } from '@/lib/useScrollAnimation'
 export default function WhyChooseImage() {
   const animRef = useScrollAnimation()
   return (
-    <div ref={animRef} className="relative w-full h-[350px] md:h-[680px] lg:h-[750px] rounded-2xl overflow-hidden shadow-2xl group">
+    <div ref={animRef} className="relative w-full h-[350px] md:h-[500px] lg:h-[500px] rounded-2xl overflow-hidden shadow-2xl group">
       <img
         src="/images/reviewsimage.webp"
         alt="Windows cleaning near me"
 
-        className="object-cover transition-transform duration-700 ease-out"
+        className="object-cover w-[100%] h-[100%] transition-transform duration-700 ease-out"
       />
     </div>
   )
